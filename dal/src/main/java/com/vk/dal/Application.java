d999246ab5e9a18bbdc5a71c6eab6e2f28328c4b
serk <serk@serk.mob.home>
2017-10-02 23:16:56 +0300

    added connection time trace method; refactored hibernate mapping

--- dal/src/main/java/com/vk/dal/Application.java
+++ dal/src/main/java/com/vk/dal/Application.java
@@ -3,38 +3,74 @@ package com.vk.dal;
 import com.vk.dal.config.MySQLAutoconfiguration;
 import com.vk.dal.domain.Customer;
 import com.vk.dal.repository.CustomerRepository;
+import org.jboss.logging.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.System.exit;
-
 public class Application {
   private static ApplicationContext applicationContext;
+  private static final Logger logger = Logger.getLogger(Application.class);
 
   static CustomerRepository customerRepository;
+  static DataSource dataSource;
 
   public static void main(String[] args) throws InterruptedException {
 
     AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MySQLAutoconfiguration.class);
 
     customerRepository = ctx.getBean(CustomerRepository.class);
+    dataSource = ctx.getBean(DataSource.class);
+
+    testConnectionCharacteristics();
     runTest();
   }
 
-  private static void runTest () throws InterruptedException {
+  private static void testConnectionCharacteristics() throws InterruptedException {
+    long opsStartTime = System.nanoTime();
+
+    try (Connection conn = dataSource.getConnection()) {
+      long opsStopTime = System.nanoTime();
+      System.out.println("Connection establish time(millis): " + TimeUnit.NANOSECONDS.toMillis(opsStopTime - opsStartTime));
+
+      try (Statement testStmt = conn.createStatement()) {
+        long stmtTestStart = System.nanoTime();
+        final String testStatement = "select 1";
+
+        testStmt.executeQuery(testStatement).close();
+        long stmtTestEnd = System.nanoTime();
+        System.out.format("Test statement '%s' exec time(millis): %d%n", testStatement, TimeUnit.NANOSECONDS.toMillis(stmtTestEnd - stmtTestStart));
+      }
+
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+      throw new InterruptedException(ex.getMessage());
+    }
+
+  }
+
+  private static void runTest() throws InterruptedException {
+
 
     long startTime = System.nanoTime();
+
+
     Customer customerJohn = customerRepository.save(new Customer("John"));
     long spentTime = System.nanoTime() - startTime;
-    System.out.println("Time spent to save one entity: " + TimeUnit.NANOSECONDS.toMillis(spentTime));
+    System.out.format("Time spent to save one entity: %d; Object id(pk): %d%n", TimeUnit.NANOSECONDS.toMillis(spentTime), customerJohn.getId());
+
 
     startTime = System.nanoTime();
     Customer customerAlice = customerRepository.save(new Customer("Alice"));
     spentTime = System.nanoTime() - startTime;
-    System.out.println("Time spent to save 2nd entity: " + TimeUnit.NANOSECONDS.toMillis(spentTime));
+    System.out.format("Time spent to save 2nd entity: %d; Object id(pk): %d%n", TimeUnit.NANOSECONDS.toMillis(spentTime), customerAlice.getId());
+
 
     Customer foundEntity = customerRepository.findOne(customerJohn.getId());
 
--- dal/src/main/java/com/vk/dal/domain/Customer.java
+++ dal/src/main/java/com/vk/dal/domain/Customer.java
@@ -1,6 +1,7 @@
 package com.vk.dal.domain;
 
 import org.hibernate.annotations.AttributeAccessor;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -9,11 +10,11 @@ import java.io.Serializable;
 @Entity
 @AttributeAccessor("field")
 @Table(name = "customer")
-public class Customer implements Serializable{
+public class Customer implements Serializable {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name="id",unique=true, nullable=false, insertable=true, updatable=true)
-  @Type(type="long")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", unique = true, nullable = false, insertable = true, updatable = true)
+  @Type(type = "long")
   private Long id;
 
   @Column(name = "name")
@@ -34,12 +35,12 @@ public class Customer implements Serializable{
     this.name = name;
   }
 
-//  @Override
+  //  @Override
   public Long getId() {
     return id;
   }
 
-//  @Override
+  //  @Override
   public void setId(Long id) {
     this.id = id;
   }
